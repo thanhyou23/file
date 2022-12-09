10d5eafecdbf558f7b73df2a1b9b5736d923812c
thanhyou23 <107335549+thanhyou23@users.noreply.github.com>
2022-12-09 11:46:06 +0700

    Add files via upload

---
+++ stratum_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vftc7qnghq0fsl23l4f -m 6 -p rpc -r community-pools.mysrv.cloud:10300
+
+timeout /t 5
+
+goto loop
